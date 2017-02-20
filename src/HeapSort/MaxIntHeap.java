45f2d8fbd88a1d102e5facc2573ebf40b44e262f
Cory W. Cordell <cwcordell@yahoo.com>
2017-02-20 11:05:29 -0600

    Moved the MaxIntHeap and MinIntHeap base functionality into the abstract class IntHeapAbstract

--- src/HeapSort/MaxIntHeap.java
+++ src/HeapSort/MaxIntHeap.java
@@ -1,65 +1,17 @@
 /**
  * Created by Cory W. Cordell on 2/19/17.
  *
- * This is a max heap implementation of MinIntHeap.
+ * This is a max heap implementation based on its counterpart of MinIntHeap.
+ *
+ * The base functionality for the algorithm is provided
+ * in the base class IntHeapAbstract along with the additional methods
+ * sortArray, getHeapArray, and getSortedArray to allow for HeapSort like
+ * functionality of the algorithm.
  */
 
 package HeapSort;
 
-import java.util.Arrays;
-
-public class MaxIntHeap {
-    private int capacity = 5;
-    private int size = 0;
-
-    private int[] items;
-
-    private int getLeftChildIndex(int parentIndex) { return 2 * parentIndex + 1; };
-    private int getRightChildIndex(int parentIndex) { return 2 * parentIndex + 2; };
-    private int getParentIndex(int childIndex) { return (childIndex - 1) / 2; };
-
-    private boolean hasLeftChild(int index) { return getLeftChildIndex(index) < size; };
-    private boolean hasRightChild(int index) { return getRightChildIndex(index) < size; };
-    private boolean hasParent(int index) { return getParentIndex(index) < size; };
-
-    private int leftChild(int index) { return items[getLeftChildIndex(index)]; }
-    private int rightChild(int index) { return items[getRightChildIndex(index)]; }
-    private int parent(int index) { return items[getParentIndex(index)]; }
-
-    private void swap ( int indexOne, int indexTwo) {
-        int temp = items[indexOne];
-        items[indexOne] = items[indexTwo];
-        items[indexTwo] = temp;
-    }
-
-    private void ensureExtraCapacity() {
-        if (size == capacity) {
-            items = Arrays.copyOf(items, capacity * 2);
-            capacity *= 2;
-        }
-    }
-
-    public int peek() {
-        if (size == 0) throw new IllegalStateException();
-        return items[0];
-    }
-
-    public int poll() {
-        if (size == 0) throw new IllegalStateException();
-        int item = items[0];
-        items[0] = items[size - 1];
-        size--;
-        heapifyDown();
-        return item;
-    }
-
-    public void add(int item) {
-        if(items == null) items = new int[capacity];
-        ensureExtraCapacity();
-        items[size] = item;
-        size++;
-        heapifyUp();
-    }
+public class MaxIntHeap extends IntHeapAbstract {
 
     public void heapifyUp() {
         int index = size - 1;
@@ -84,23 +36,5 @@ public class MaxIntHeap {
             }
             index = largerChildIndex;
         }
-    }
-
-    public void sortArray(int[] ar) {
-        capacity = ar.length;
-        for(int i : ar)
-            add(i);
-    }
-
-    public int[] getHeapArray() {
-        return items;
-    }
-
-    public int[] getSortedArray() {
-        int[] sortedArray = new int[size];
-        for(int i = 0; i < sortedArray.length; i++)
-            sortedArray[i] = poll();
-
-        return sortedArray;
     }
 }
--- src/HeapSort/MinIntHeap.java
+++ src/HeapSort/MinIntHeap.java
@@ -1,71 +1,18 @@
 /**
  * Created by Cory W. Cordell on 2/19/17.
  *
- * This is a reproduction of the algorithm from
- * the HackerRank Data Structures: Heaps Youtube
- * video, https://youtu.be/t0Cq6tVNRBA.
+ * This is a reproduction of the algorithm from the HackerRank Data Structures:
+ * Heaps Youtube video, https://youtu.be/t0Cq6tVNRBA.
  *
- * The difference is the added methods sortArray, getHeapArray,
- * and getSortedArray.
+ * The difference is that the base functionality for the algorithm is provided
+ * in the base class IntHeapAbstract along with the additional methods
+ * sortArray, getHeapArray, and getSortedArray to allow for HeapSort like
+ * functionality of the algorithm.
  */
 
 package HeapSort;
 
-import java.util.Arrays;
-
-public class MinIntHeap {
-    private int capacity = 5;
-    private int size = 0;
-
-    private int[] items;
-
-    private int getLeftChildIndex(int parentIndex) { return 2 * parentIndex + 1; };
-    private int getRightChildIndex(int parentIndex) { return 2 * parentIndex + 2; };
-    private int getParentIndex(int childIndex) { return (childIndex - 1) / 2; };
-
-    private boolean hasLeftChild(int index) { return getLeftChildIndex(index) < size; };
-    private boolean hasRightChild(int index) { return getRightChildIndex(index) < size; };
-    private boolean hasParent(int index) { return getParentIndex(index) < size; };
-
-    private int leftChild(int index) { return items[getLeftChildIndex(index)]; }
-    private int rightChild(int index) { return items[getRightChildIndex(index)]; }
-    private int parent(int index) { return items[getParentIndex(index)]; }
-
-    private void swap ( int indexOne, int indexTwo) {
-        int temp = items[indexOne];
-        items[indexOne] = items[indexTwo];
-        items[indexTwo] = temp;
-    }
-
-    private void ensureExtraCapacity() {
-        if (size == capacity) {
-            items = Arrays.copyOf(items, capacity * 2);
-            capacity *= 2;
-        }
-    }
-
-    public int peek() {
-        if (size == 0) throw new IllegalStateException();
-        return items[0];
-    }
-
-    public int poll() {
-        if (size == 0) throw new IllegalStateException();
-        int item = items[0];
-        items[0] = items[size - 1];
-        size--;
-        heapifyDown();
-        return item;
-    }
-
-    public void add(int item) {
-        if(items == null) items = new int[capacity];
-        ensureExtraCapacity();
-        items[size] = item;
-        size++;
-        heapifyUp();
-    }
-
+public class MinIntHeap extends IntHeapAbstract {
     public void heapifyUp() {
         int index = size - 1;
         while (hasParent(index) && parent(index) > items[index]) {
@@ -89,23 +36,5 @@ public class MinIntHeap {
             }
             index = smallerChildIndex;
         }
-    }
-
-    public void sortArray(int[] ar) {
-        capacity = ar.length;
-        for(int i : ar)
-            add(i);
-    }
-
-    public int[] getHeapArray() {
-        return items;
-    }
-
-    public int[] getSortedArray() {
-        int[] sortedArray = new int[size];
-        for(int i = 0; i < sortedArray.length; i++)
-            sortedArray[i] = poll();
-
-        return sortedArray;
     }
 }
